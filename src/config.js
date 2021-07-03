0032a7a725c9f87019b6e748998e0d2ad171ad9b
Emanuele <emanuelelena021099@gmail.com>
2021-07-03 10:27:07 +0200

    config for first test

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 var config = {
     "mqtt": {
-        "address": "", 
+        "address": "mqtt://192.168.1.133:8883", 
         "topics": {
             "left": "/distances/left", 
             "center": "/distances/center", 
@@ -8,7 +8,7 @@ var config = {
         }
     }, 
     "video": {
-        "address": "", 
+        "address": "https://images2-corrieredelveneto.corriereobjects.it/methode_image/2021/03/23/Treviso%20Belluno/Foto%20Trattate/accoltellamento%20mogliano-kHv-U460802138672084hjB-640x480@CorriereVeneto-Web-Veneto.jpg?v=202103240900", 
         "width": 640,
         "height": 480
     }
