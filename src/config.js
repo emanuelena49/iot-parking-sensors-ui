1ee199a88cd9c6385bd76ab94e81f9c45cafc115
Alex Dumitru <aaaalexdumitru@gmail.com>
2021-07-06 15:11:40 +0200

    Update condif

--- src/config.js
+++ src/config.js
@@ -2,13 +2,14 @@ var config = {
     "mqtt": {
         "address": "mqtt://192.168.1.133:8883", 
         "topics": {
-            "left": "/distances/left", 
-            "center": "/distances/center", 
-            "right": "/distances/right"
+            "left": "ultrasonic/0", 
+            "center": "ultrasonic/1", 
+            "right": "ultrasonic/2"
         }
     }, 
     "video": {
-        "address": "https://images2-corrieredelveneto.corriereobjects.it/methode_image/2021/03/23/Treviso%20Belluno/Foto%20Trattate/accoltellamento%20mogliano-kHv-U460802138672084hjB-640x480@CorriereVeneto-Web-Veneto.jpg?v=202103240900", 
+        //"address": "https://images2-corrieredelveneto.corriereobjects.it/methode_image/2021/03/23/Treviso%20Belluno/Foto%20Trattate/accoltellamento%20mogliano-kHv-U460802138672084hjB-640x480@CorriereVeneto-Web-Veneto.jpg?v=202103240900", 
+        "address": "http://192.168.1.133:8081",
         "width": 640,
         "height": 480
     }
